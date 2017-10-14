7565b5476207b1f9b1ccdbd7dd3204c98de8f2b0
Freshek <wsrh456@gmail.com>
2017-10-14 19:04:58 +0200

    cool windows minimize

--- src/js/WindowFactory.js
+++ src/js/WindowFactory.js
@@ -8,20 +8,46 @@ class WindowFactory {
       width: params.width != null ? params.width : 400,
       height: params.height != null ? params.height + 40: 300,
     });
+
+    var content = jQuery("<div/>", {
+      width: params.width,
+      height: params.height
+    });
+
     var header = jQuery("<h4/>", {
       width: "100%",
       height: "40px",
       text: params.text != "" ? params.text : "window"
     });
 
-    div.css({backgroundColor: "rgba(25, 25, 25, 0.8)", position: "absolute"});
+    var minimizeBtn = jQuery("<h4/>", {
+      height: "40px",
+      text: "–"
+    })
+
+    div.css({backgroundColor: "transparent", position: "absolute"});
     div.appendTo("body");
 
     header.css({backgroundColor: "rgba(25, 25, 25, 0.9)", top: 0, left: 0, padding: "5px", boxSizing: "border-box"});
     header.appendTo(div);
 
+    minimizeBtn.css({top: 0, right: 0, padding: "10px", position: "absolute"});
+    minimizeBtn.appendTo(div);
+
+    content.css({backgroundColor: "rgba(25, 25, 25, 0.8)"});
+    content.appendTo(div);
+
+    minimizeBtn.click(function() {
+      if (content.css("display") !== "none") {
+        content.fadeOut(500);
+      }
+      else {
+        content.fadeIn(500);
+      }
+    });
+
     div.draggable({handle: header});
 
-    return div;
+    return content;
   }
 }
