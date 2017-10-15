e1e75d351f80a5d482ebb8fc24bbc8b3ecea252d
Freshek <wsrh456@gmail.com>
2017-10-15 14:04:03 +0200

    code cleanup

--- src/js/objects/Ship.js
+++ src/js/objects/Ship.js
@@ -3,7 +3,7 @@ Created by Freshek on 15.10.2017
 */
 
 class Ship extends Movable {
-  constructor(id, x, y, isnpc, name) {
+  constructor(x, y, id, isnpc, name) {
     super(x, y);
     this.id = id;
     this.isNpc = isnpc;
