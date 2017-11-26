964effd421fa60723a5430ef85f5d192a27b24aa
BorisKolyarov <9309706@gmail.com>
2017-11-26 23:29:04 +0200

    the ability to disable the runtime display has been added to the global settings

--- src/js/windows/StatisticWindow.js
+++ src/js/windows/StatisticWindow.js
@@ -1,8 +1,8 @@
 class StatisticWindow {
 
-    createWindow(){
+    createWindow() {
         this.botStatisticWindow = WindowFactory.createWindow({width: 300, text: "Statistic"});
-        this.connected=false;
+        this.connected = false;
         let startTime = new Date();
 
         let options = [
@@ -10,12 +10,6 @@ class StatisticWindow {
                 name: 'startTime',
                 labelText: 'Start at: ',
                 spanText: startTime.toLocaleString(navigator.languages[0]),
-                appendTo: this.botStatisticWindow
-            },
-            {
-                name: 'runtime',
-                labelText: 'Runtime: ',
-                spanText: '00:00:00',
                 appendTo: this.botStatisticWindow
             },
             {
@@ -62,42 +56,53 @@ class StatisticWindow {
             },
         ];
 
-        options.forEach((option)=>{
+        if (window.globalSettings.showRuntime) {
+            options.push({
+                name: 'runtime',
+                labelText: 'Runtime: ',
+                spanText: '00:00:00',
+                appendTo: this.botStatisticWindow
+            });
+        }
+
+        options.forEach((option)=> {
             this[option.name] = ControlFactory.info(option);
         });
 
         let standardListeners = [
-            {event: 'addCredits', el: 'credits', detailEl:'credits'},
-            {event: 'addUridium', el: 'uridium', detailEl:'uridium'},
-            {event: 'addGgEnergy', el: 'energy', detailEl:'energy'},
-            {event: 'addAmmo', el: 'ammo', detailEl:'ammo'},
-            {event: 'addExperience', el: 'experience', detailEl:'experience'},
-            {event: 'addHonor', el: 'honor', detailEl:'honor'},
+            {event: 'addCredits', el: 'credits', detailEl: 'credits'},
+            {event: 'addUridium', el: 'uridium', detailEl: 'uridium'},
+            {event: 'addGgEnergy', el: 'energy', detailEl: 'energy'},
+            {event: 'addAmmo', el: 'ammo', detailEl: 'ammo'},
+            {event: 'addExperience', el: 'experience', detailEl: 'experience'},
+            {event: 'addHonor', el: 'honor', detailEl: 'honor'},
         ];
 
-        standardListeners.forEach((item)=>{
+        standardListeners.forEach((item)=> {
             this.setStandardEventListener(item);
         });
 
-        $(window).on('connection', (e)=>{
+        $(window).on('connection', (e)=> {
             this.connected = e.detail.connected;
         });
 
-        $(window).on('logicEnd', ()=>{
-            if(this.connected) {
+        $(window).on('logicEnd', ()=> {
+            if (this.connected) {
                 let uri = parseInt($('span:last-child', this.uridium).html());
-                $('span:last-child', this.runtime).text(  TimeHelper.diff(startTime) );
-				$('span:last-child', this.speed).text( (uri ? ( uri  /  TimeHelper.totatalMinutes(startTime)).toFixed(2):'0.00') + '  uri/min.' );
+                if (window.globalSettings.showRuntime) {
+                    $('span:last-child', this.runtime).text(TimeHelper.diff(startTime));
+                }
+                $('span:last-child', this.speed).text((uri ? ( uri / TimeHelper.totatalMinutes(startTime)).toFixed(2) : '0.00') + '  uri/min.');
             }
         });
     }
 
-    setStandardEventListener({event, el, detailEl}){
+    setStandardEventListener({event, el, detailEl}) {
         let htmlEl = this[el];
-        $(window).on(event, (e)=>{
+        $(window).on(event, (e)=> {
             let el = $('span:last-child', htmlEl);
             let collected = parseInt(el.html());
-            el.text(parseInt(e.detail[detailEl])+collected);
+            el.text(parseInt(e.detail[detailEl]) + collected);
         });
     }
 }
